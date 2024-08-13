229a4886284eb7240618dcf9b491c76043912250
Emil Aagaard <emil@eacit.dk>
2024-08-13 23:45:01 +0200

    Added link to examples folder

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,8 @@
 //! - are time-reversal invariant.
 //! - are reflection invariant.
 //! - are translation invariant.
-//! See the ``examples`` folder for calculation of the ground state energies and spectral gaps of the S=1 antiferromagnetic Heisenberg chain and the Affleck-Kennedy-Lieb-Tasaki chain.
+//! 
+//! See the [``examples``](https://github.com/emil-aagaard/quantum-spin-chains/tree/main/examples) folder for calculation of the ground state energies and spectral gaps of the S=1 antiferromagnetic Heisenberg chain and the Affleck-Kennedy-Lieb-Tasaki chain.
 pub mod symmetries;
 pub mod basis;
 pub mod states;
